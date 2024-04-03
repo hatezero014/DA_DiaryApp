60be23b986682aa00c6131c0477e5e92f354e4e6
Duc Minh Nguyen <ngdm271@gmail.com>
2024-04-03 11:24:12 +0700

    Making changes to fragments in MainActivity2

--- app/src/main/java/com/example/doan_diaryapp/ui/analyze/AnalyzeFragment.java
+++ app/src/main/java/com/example/doan_diaryapp/ui/analyze/AnalyzeFragment.java
@@ -1,4 +1,4 @@
-package com.example.doan_diaryapp.ui.notifications;
+package com.example.doan_diaryapp.ui.analyze;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,18 +10,18 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.doan_diaryapp.databinding.FragmentNotificationsBinding;
+import com.example.doan_diaryapp.databinding.FragmentAnalyzeBinding;
 
-public class NotificationsFragment extends Fragment {
+public class AnalyzeFragment extends Fragment {
 
-    private FragmentNotificationsBinding binding;
+    private FragmentAnalyzeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        AnalyzeViewModel notificationsViewModel =
+                new ViewModelProvider(this).get(AnalyzeViewModel.class);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        binding = FragmentAnalyzeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textNotifications;
--- app/src/main/java/com/example/doan_diaryapp/ui/collection/CollectionFragment.java
+++ app/src/main/java/com/example/doan_diaryapp/ui/collection/CollectionFragment.java
@@ -1,4 +1,4 @@
-package com.example.doan_diaryapp.ui.dashboard;
+package com.example.doan_diaryapp.ui.collection;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,23 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.doan_diaryapp.databinding.FragmentDashboardBinding;
+import com.example.doan_diaryapp.databinding.FragmentCollectionBinding;
 
-public class DashboardFragment extends Fragment {
+public class CollectionFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentCollectionBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        CollectionViewModel dashboardViewModel =
+                new ViewModelProvider(this).get(CollectionViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentCollectionBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textDashboard;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-
         return root;
     }
 
