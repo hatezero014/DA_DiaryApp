fc11d9bff25c2fde3890b9abeb638ba6c4f2b830
vanphong19 <phongdv1901@gmail.com>
2024-04-06 14:16:03 +0700

    thietke

---
+++ app/src/main/java/com/example/doan_diaryapp/Models/Circles.java
@@ -0,0 +1,20 @@
+package com.example.doan_diaryapp.Models;
+
+import java.util.List;
+
+public class Circles {
+
+    private List<Circle> mCircle;
+
+    public Circles(List<Circle> mCircle) {
+        this.mCircle = mCircle;
+    }
+
+    public List<Circle> getmCircle() {
+        return mCircle;
+    }
+
+    public void setmCircle(List<Circle> mCircle) {
+        this.mCircle = mCircle;
+    }
+}
