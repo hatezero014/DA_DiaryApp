6f2e7180f5639f26ad8d83dc5814ffde32888d0a
ASUS PC <22520927@gm.uit.edu.vn>
2024-04-07 18:12:25 +0700

    codeMonth

--- app/src/main/java/com/example/doan_diaryapp/ui/home/MonthFragment.java
+++ app/src/main/java/com/example/doan_diaryapp/ui/home/MonthFragment.java
@@ -10,11 +10,17 @@ import androidx.navigation.fragment.NavHostFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 import com.example.doan_diaryapp.R;
 import com.example.doan_diaryapp.databinding.FragmentHomeBinding;
 import com.example.doan_diaryapp.databinding.FragmentMonthBinding;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 
 public class MonthFragment extends Fragment {
@@ -23,11 +29,43 @@ public class MonthFragment extends Fragment {
     private FragmentHomeBinding bindingHome;
 
     @Override
-    public View onCreateView(
-            @NonNull LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_month, container, false);
+        ButtonAddMonth(view);
+        setCardViewDate(view);
+        return view;
+    }
+
+
+
+    private void setCardViewDate(View view)
+    {
+        CalendarView calendarView = view.findViewById(R.id.calendarView);
+        TextView selectedDateTextView = view.findViewById(R.id.CardViewDate);
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE, dd-MM-yyyy", new Locale("vi", "VN"));
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        String currentDate = sdf.format(calendar.getTime());
+        selectedDateTextView.setText(currentDate);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+
+                Calendar selectedCalendar = Calendar.getInstance();
+                selectedCalendar.set(year, month, dayOfMonth);
+                String selectedDate = sdf.format(selectedCalendar.getTime());
+                selectedDateTextView.setText(selectedDate);
+            }
+        });
+    }
+
+
+
+    private void ButtonAddMonth(View view)
+    {
         FloatingActionButton buttonAddMonth = view.findViewById(R.id.ButtonAddMonth);
         buttonAddMonth.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,9 +73,9 @@ public class MonthFragment extends Fragment {
                 showAlertDialog();
             }
         });
-
-        return view;
     }
+
+
 
     private void showAlertDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
