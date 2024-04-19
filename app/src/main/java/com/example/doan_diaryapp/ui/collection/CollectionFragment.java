ca266da071f87725bd79200588585266632444cb
ASUS PC <22520927@gm.uit.edu.vn>
2024-04-19 07:56:06 +0700

    codeListDayQT

--- app/src/main/java/com/example/doan_diaryapp/ui/collection/CollectionFragment.java
+++ app/src/main/java/com/example/doan_diaryapp/ui/collection/CollectionFragment.java
@@ -14,7 +14,11 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.doan_diaryapp.Adapter.EntryAdapter;
+import com.example.doan_diaryapp.Models.Entry;
 import com.example.doan_diaryapp.R;
+import com.example.doan_diaryapp.Service.EntryService;
+import com.example.doan_diaryapp.Service.ImportantDayService;
 import com.example.doan_diaryapp.databinding.FragmentCollectionBinding;
 
 import java.util.ArrayList;
@@ -24,16 +28,40 @@ public class CollectionFragment extends Fragment {
     private FragmentCollectionBinding binding;
     private ArrayList<CarouselModel> list = new ArrayList<>();
     private CarouselAdapter carouselAdapter;
+    private ListView mListView;
+    private EntryAdapter mAdapter;
+    private ImportantDayService importantDayService;
+
+    private void updateEntries() {
+        List<Entry> entryList = importantDayService.getEntriesFromDatabaseQT();
+        mAdapter.clear();
+        mAdapter.addAll(entryList);
+        mAdapter.notifyDataSetChanged();
+    }
+
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentCollectionBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
+        ViewPhoto(view);
+
+        mListView = view.findViewById(R.id.ListDayQT);
+        importantDayService = new ImportantDayService(getContext());
+        List<Entry> entryList = importantDayService.getEntriesFromDatabaseQT();
+        mAdapter = new EntryAdapter(getContext(), entryList);
+        mListView.setAdapter(mAdapter);
+
+        return view;
+    }
+
+    private void ViewPhoto(View view)
+    {
         RecyclerView recyclerView = view.findViewById(R.id.carousel_recycler_view);
         ViewGroup.LayoutParams layoutParams = recyclerView.getLayoutParams();
         layoutParams.height = (int) getResources().getDimension(R.dimen.recyclerview_height);
         recyclerView.setLayoutParams(layoutParams);
-
         carouselAdapter = new CarouselAdapter(list, requireContext());
         recyclerView.setAdapter(carouselAdapter);
 
@@ -47,23 +75,15 @@ public class CollectionFragment extends Fragment {
         list.add(new CarouselModel(R.drawable.i3));
         list.add(new CarouselModel(R.drawable.i4));
         list.add(new CarouselModel(R.drawable.i5));
-
-
-        ListView listView = view.findViewById(R.id.ListDayQT);
-        listView.setAdapter(new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_2, android.R.id.text1, generateItemList()));
-
-
-        return view;
     }
 
 
-    private List<String> generateItemList() {
-        List<String> itemList = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            itemList.add("Ngày quan trọng " + (i + 1) + "\nSự kiện xảy ra " + (i + 1));
-        }
-        return itemList;
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateEntries();
     }
+
 
 
     @Override
