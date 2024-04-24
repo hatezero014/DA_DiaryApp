e0b769bf61949d8955c4961afafc032c58ef2d6a
NamLe <lexuannam6426@gmail.com>
2024-04-24 12:08:15 +0700

    fix setting

--- app/src/main/java/com/example/doan_diaryapp/SettingActivity.java
+++ app/src/main/java/com/example/doan_diaryapp/SettingActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
