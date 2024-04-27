0502f9ab9eb45f115eb377b77519027e88171f49
MinhNoProUIT <minh8dclv123@gmail.com>
2024-04-27 13:17:09 +0700

    Setting(Update)

--- app/src/main/java/com/example/doan_diaryapp/NotificationHelper.java
+++ app/src/main/java/com/example/doan_diaryapp/NotificationHelper.java
@@ -13,7 +13,7 @@ public class NotificationHelper {
     private static final String CHANNEL_ID = "your_channel_id";
     private static final String CHANNEL_NAME = "Your Channel Name";
     private static final String CHANNEL_DESCRIPTION = "Your Channel Description";
-    private static final int NOTIFICATION_ID = 1;
+    public static final int NOTIFICATION_ID = 1;
 
     public static void createNotificationChannel(Context context) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -35,42 +35,40 @@ public class NotificationHelper {
     }
 
     public static void showNotification(Context context, String title, String content) {
-        // Check if the app has permission to show notifications
         if (NotificationManagerCompat.from(context).areNotificationsEnabled()) {
-            // Proceed with showing the notification
             NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
-                    .setSmallIcon(R.drawable.emoji_acitivity_travel)
+                    .setSmallIcon(R.drawable.icon_noti)
                     .setContentTitle(title)
                     .setContentText(content)
                     .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                     .setAutoCancel(true);
 
-            // Create an explicit intent for an Activity in your app.
             Intent intent = new Intent(context, ChangeReminderTime.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
             builder.setContentIntent(pendingIntent);
 
+            Intent intent1 = new Intent(context, ChangeReminderTime.class);
+            intent1.setAction("android.intent.action.VIEW"); // Thêm action để xác định khi người dùng nhấn vào thông báo
+            intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingIntent1 = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+            builder.setContentIntent(pendingIntent1);
+
+
+
             NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
             try {
                 notificationManager.notify(NOTIFICATION_ID, builder.build());
             } catch (SecurityException e) {
-                // Handle the SecurityException
                 e.printStackTrace();
-                // You can log the exception or perform any other necessary action
             }
         } else {
-            // Handle the case where the app does not have permission to show notifications
-            // You can prompt the user to grant notification permission here
-            // For example, you can show a dialog or navigate the user to app settings
             Intent intent = new Intent();
             intent.setAction("android.settings.APP_NOTIFICATION_SETTINGS");
 
-            // For Android 5 and below
             intent.putExtra("app_package", context.getPackageName());
             intent.putExtra("app_uid", context.getApplicationInfo().uid);
 
-            // For Android 6 and above
             intent.putExtra("android.provider.extra.APP_PACKAGE", context.getPackageName());
 
             context.startActivity(intent);
