c08194aefc104ef40853375256cec38d1c0b592b
MinhNoProUIT <minh8dclv123@gmail.com>
2024-05-22 00:02:35 +0700

    ReminderNotification

---
+++ app/src/main/java/com/example/doan_diaryapp/NotificationReceiver.java
@@ -0,0 +1,42 @@
+package com.example.doan_diaryapp;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import androidx.core.content.ContextCompat;
+
+import static android.Manifest.permission.POST_NOTIFICATIONS;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class NotificationReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent i = new Intent(context, MainActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, i, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, context.getString(R.string.id_NotificationReceiver))
+                .setSmallIcon(R.drawable.icon_noti)
+                .setContentTitle(context.getString(R.string.remind_notification))
+                .setContentText(context.getString(R.string.remind_notification_desciption))
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        if (ContextCompat.checkSelfPermission(context, POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+            notificationManager.notify(123, builder.build());
+        } else {
+
+        }
+
+    }
+}
